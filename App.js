86c7e1be763dc0ca4be47bd3044115a597550675
LolipopEater <81711726+LolipopEater@users.noreply.github.com>
2022-05-05 08:37:24 +0300

    navigation implementation

--- App.js
+++ App.js
@@ -3,11 +3,59 @@ import React from "react";
 import { ThemeProvider } from "styled-components/native";
 import { theme } from "./src/infrastructure/theme";
 import { ResterauntsScreen } from "./src/features/resteraunts/screens/resteraunts.screen";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Text } from "react-native";
 import {
   useFonts as useOswald,
   Oswald_400Regular,
 } from "@expo-google-fonts/oswald";
 import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
+import { SafeArea } from "./src/components/utility/safe-area.component";
+import Ionicons from "@expo/vector-icons/Ionicons";
+const Tab = createBottomTabNavigator();
+
+const MapScreen = () => {
+  return (
+    <SafeArea>
+      <Text>Home!</Text>
+    </SafeArea>
+  );
+};
+
+const SettingsScreen = () => {
+  return (
+    <SafeArea>
+      <Text>Settings!</Text>
+    </SafeArea>
+  );
+};
+const HomeScreen = () => {
+  return (
+    <SafeArea>
+      <ResterauntsScreen />
+    </SafeArea>
+  );
+};
+
+const TAB_ICON = {
+  Resteraunts: "ios-fast-food",
+  Settings: "ios-list-circle",
+  Map: "map-sharp",
+};
+
+const screenOptions = ({ route }) => {
+  const iconName = TAB_ICON[route.name];
+
+  return {
+    tabBarIcon: ({ size, color }) => (
+      <Ionicons name={iconName} size={size} color={color} />
+    ),
+    tabBarActiveTintColor: "purple",
+    tabBarInactiveTintColor: "gray",
+  };
+};
+
 export default function App() {
   const [oswaldLoaded, error] = useOswald({
     Oswald_400Regular,
@@ -19,9 +67,17 @@ export default function App() {
     return null;
   }
   return (
-    <ThemeProvider theme={theme}>
-      <ResterauntsScreen />
+    <>
+      <ThemeProvider theme={theme}>
+        <NavigationContainer>
+          <Tab.Navigator screenOptions={screenOptions}>
+            <Tab.Screen name="Resteraunts" component={HomeScreen} />
+            <Tab.Screen name="Map" component={MapScreen} />
+            <Tab.Screen name="Settings" component={SettingsScreen} />
+          </Tab.Navigator>
+        </NavigationContainer>
+      </ThemeProvider>
       <ExpoStatusBar style="auto" />
-    </ThemeProvider>
+    </>
   );
 }
--- src/features/resteraunts/screens/resteraunts.screen.js
+++ src/features/resteraunts/screens/resteraunts.screen.js
@@ -1,13 +1,10 @@
 import React from "react";
-import { StatusBar, StyleSheet, SafeAreaView } from "react-native";
+import { FlatList } from "react-native";
 import { Searchbar } from "react-native-paper";
 import { RestaurantInfoCard } from "../components/resteraunt-info.card";
 import styled from "styled-components/native";
+import { Spacer } from "../../../components/spacer/spacer.component";
 
-const SafeArea = styled(SafeAreaView)`
-  flex: 1;
-  ${StatusBar.currentHeight && `margin-top: ${StatusBar.currentHeight}px`};
-`;
 const SearchContainer = styled.View`
   padding: ${(props) => props.theme.space[3]};
 `;
@@ -17,14 +14,20 @@ const RestaurantListContainer = styled.View`
   padding: ${(props) => props.theme.space[3]};
   background-color: ${(props) => props.theme.colors.ui.quaternary};
 `;
-
+const RestaurantList = styled(FlatList)``;
 export const ResterauntsScreen = () => (
-  <SafeArea>
+  <>
     <SearchContainer>
       <Searchbar />
     </SearchContainer>
-    <RestaurantListContainer>
-      <RestaurantInfoCard />
-    </RestaurantListContainer>
-  </SafeArea>
+    <RestaurantList
+      data={[{ name: 1 }, { name: 2 }, { name: 3 }, { name: 4 }]}
+      renderItem={() => (
+        <Spacer position="bottom" size="large">
+          <RestaurantInfoCard />
+        </Spacer>
+      )}
+      keyExtractor={(item) => item.name}
+    />
+  </>
 );
