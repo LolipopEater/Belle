e0300f6e386e790f58cd2707ff39c9cc62cc0250
LolipopEater <81711726+LolipopEater@users.noreply.github.com>
2022-11-09 13:03:52 +0200

    first commit

--- src/utils/env.js
+++ src/utils/env.js
@@ -1,6 +1,6 @@
 const liveHost = "https://us-central1-mealstogo-362f3.cloudfunctions.net";
 const localHost =
-  "https://a309-77-125-117-90.eu.ngrok.io/mealstogo-362f3/us-central1";
+  "https://ff76-77-125-116-235.eu.ngrok.io/mealstogo-362f3/us-central1";
 
 export const isDevelopment = process.env.NODE_ENV === "development";
 export const isMock = true;
