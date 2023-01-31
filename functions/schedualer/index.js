119aa407f154c3685f52d567feff12f390dda9a3
LolipopEater <81711726+LolipopEater@users.noreply.github.com>
2023-01-31 17:13:24 +0200

    SchedulerRequest-DONE

--- functions/schedualer/index.js
+++ functions/schedualer/index.js
@@ -9,12 +9,46 @@ admin.initializeApp({
   databaseURL: "https://mealstogo-362f3-default-rtdb.firebaseio.com",
 });
 
-exports.getSchedualer = functions.firestore
-  .document("Feed/{FeedDoc}/comments/{commentDoc}")
-  .onCreate(async (snap, context) => {
-    const commentData = snap.data();
+exports.getSchedualer = functions.https.onRequest(async (request, response) => {
+  const { caregiverId, month, day } = url.parse(request.url, true).query; //extract the get requst by ID
+  if (!caregiverId) {
+    return response.status(400).send({ error: "caregiverId is required" });
+  }
 
-    const feedDocId = context.params.FeedDoc;
-    const feedSnap = await admin.firestore().doc(`Feed/${FeedDocId}`).get();
-    // ...
-  });
+  try {
+    const caregiverRef = admin
+      .firestore()
+      .collection("caregivers")
+      .doc(caregiverId);
+
+    const subCollectionData = [];
+    const appointments = []; //show all appointments in db that day
+
+    const caregiverSnapshot = await caregiverRef.get();
+
+    subCollectionData.push(caregiverSnapshot.data());
+
+    const dayRef = admin
+      .firestore()
+      .collection("caregivers")
+      .doc(caregiverId)
+      .collection(caregiverId + "-p")
+      .doc(month);
+
+    const daySnapShot = await dayRef.collection(day).get();
+
+    daySnapShot.forEach((doc) => {
+      appointments.push(doc.data());
+    });
+
+    subCollectionData.push(appointments);
+
+    if (!caregiverSnapshot.exists) {
+      return response.status(404).send({ error: "Caregiver not found" });
+    }
+
+    return response.send(subCollectionData);
+  } catch (error) {
+    return response.status(500).send({ error: error.message });
+  }
+});
